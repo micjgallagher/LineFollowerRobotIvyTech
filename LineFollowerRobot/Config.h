34ca0a710c5ac629495acbd9a26efc5c5b3a2f9e
Duncan Brasher <dbrasher5@ivytech.edu>
2025-02-28 17:52:31 -0500

    Implement interval for `DEBUG_PRINT` so the console is not flooded.

--- LineFollowerRobot/Config.h
+++ LineFollowerRobot/Config.h
@@ -1,3 +1,4 @@
 #define DEBUG_PRINT true
+#define DEBUG_PRINT_INTERVAL 20
 #define DEBUG_MOTORS false
 
--- LineFollowerRobot/LineFollowerRobot.ino
+++ LineFollowerRobot/LineFollowerRobot.ino
@@ -1,6 +1,8 @@
 // Options
 #include "Config.h"
 
+int DEBUG_PRINT_SENTINEL=0;
+
 // Speeds
 #define MOTOR_SPEED 180 // Reverse sign as needed.
 
@@ -21,69 +23,77 @@ int leftMotorPin2=10;
 
 void setup()
 {
-  Serial.begin(9600); 
-  //The problem with TT gear motors is that, at very low pwm value it does not even rotate.
-  //If we increase the PWM value then it rotates faster and our robot is not controlled in that speed and goes out of line.
-  //For that we need to increase the frequency of analogWrite.
-  //Below line is important to change the frequency of PWM signal on pin D5 and D6
-  //Because of this, motor runs in controlled manner (lower speed) at high PWM value.
-  //This sets frequency as 7812.5 hz.
-  //TCCR0B = TCCR0B & B11111000 | B00000010;
+    Serial.begin(9600); 
+    //The problem with TT gear motors is that, at very low pwm value it does not even rotate.
+    //If we increase the PWM value then it rotates faster and our robot is not controlled in that speed and goes out of line.
+    //For that we need to increase the frequency of analogWrite.
+    //Below line is important to change the frequency of PWM signal on pin D5 and D6
+    //Because of this, motor runs in controlled manner (lower speed) at high PWM value.
+    //This sets frequency as 7812.5 hz.
+    //TCCR0B = TCCR0B & B11111000 | B00000010;
 
-  // put your setup code here, to run once:
-  pinMode(enableRightMotor, OUTPUT);
-  pinMode(rightMotorPin1, OUTPUT);
-  pinMode(rightMotorPin2, OUTPUT);
-  
-  pinMode(enableLeftMotor, OUTPUT);
-  pinMode(leftMotorPin1, OUTPUT);
-  pinMode(leftMotorPin2, OUTPUT);
+    // put your setup code here, to run once:
+    pinMode(enableRightMotor, OUTPUT);
+    pinMode(rightMotorPin1, OUTPUT);
+    pinMode(rightMotorPin2, OUTPUT);
 
-  pinMode(IR_SENSOR_RIGHT, INPUT);
-  pinMode(IR_SENSOR_LEFT, INPUT);
-  rotateMotors(0,0);   
+    pinMode(enableLeftMotor, OUTPUT);
+    pinMode(leftMotorPin1, OUTPUT);
+    pinMode(leftMotorPin2, OUTPUT);
+
+    pinMode(IR_SENSOR_RIGHT, INPUT);
+    pinMode(IR_SENSOR_LEFT, INPUT);
+    rotateMotors(0,0);   
 }
 
 
 void loop()
 {
 
-  int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
-  int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+    int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
+    int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
 
-  if (DEBUG_PRINT) {
-    Serial.print("rightIRSensorValue = ");
-    Serial.println(rightIRSensorValue, DEC);
-    Serial.print("leftIRSensorValue = ");
-    Serial.print(leftIRSensorValue, DEC);
-    Serial.println();
-  }
+    if (DEBUG_PRINT) {
+        if (DEBUG_PRINT_SENTINEL == 0) {
+            Serial.print("rightIRSensorValue = ");
+            Serial.println(rightIRSensorValue, DEC);
+            Serial.print("leftIRSensorValue = ");
+            Serial.print(leftIRSensorValue, DEC);
+            Serial.println();
+        }
 
-  if (DEBUG_MOTORS) {
-      rotateMotors(MOTOR_SPEED, MOTOR_SPEED);
-      return;
-  }
+        if (DEBUG_PRINT_SENTINEL >= DEBUG_PRINT_INTERVAL) { // This is carefully constructed so that all users of the sentinel can simply check for equality to zero and do not need to consider incrementation/reset and the cycle will begin on the first run of the loop instead of skipping until after the first interval.
+            DEBUG_PRINT_SENTINEL = 0;
+        } else {
+            DEBUG_PRINT_SENTINEL += 1;
+        }
+    }
 
-  //If none of the sensors detects black line, then go straight
-  if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
-  {
-    rotateMotors(MOTOR_SPEED, MOTOR_SPEED);
-  }
-  //If right sensor detects black line, then turn right
-  else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
-  {
-      rotateMotors(-MOTOR_SPEED, MOTOR_SPEED); 
-  }
-  //If left sensor detects black line, then turn left  
-  else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
-  {
-      rotateMotors(MOTOR_SPEED, -MOTOR_SPEED); 
-  } 
-  //If both the sensors detect black line, then stop 
-  else 
-  {
-    rotateMotors(0, 0);
-  }
+    if (DEBUG_MOTORS) {
+        rotateMotors(MOTOR_SPEED, MOTOR_SPEED);
+        return;
+    }
+
+    //If none of the sensors detects black line, then go straight
+    if (rightIRSensorValue == LOW && leftIRSensorValue == LOW)
+    {
+        rotateMotors(MOTOR_SPEED, MOTOR_SPEED);
+    }
+    //If right sensor detects black line, then turn right
+    else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW )
+    {
+        rotateMotors(-MOTOR_SPEED, MOTOR_SPEED); 
+    }
+    //If left sensor detects black line, then turn left  
+    else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH )
+    {
+        rotateMotors(MOTOR_SPEED, -MOTOR_SPEED); 
+    } 
+    //If both the sensors detect black line, then stop 
+    else 
+    {
+        rotateMotors(0, 0);
+    }
 }
 
 
@@ -95,34 +105,37 @@ void rotateMotors(int rightMotorSpeed, int leftMotorSpeed)
 
 void rotateMotor(int speed, int pin1, int pin2, int enable)
 {
-  if (DEBUG_PRINT) {
-    Serial.print("Turning motor with pin1 = ");
-    Serial.print(pin1, DEC);
-    Serial.print(" , pin2 = ");
-    Serial.print(pin2, DEC);
-    Serial.print(" , and enable pin = ");
-    Serial.print(enable, DEC);
-    Serial.print(" with speed = ");
-    Serial.print(speed, DEC);
-    Serial.println();
-  }
+    if (DEBUG_PRINT) {
+        if (DEBUG_PRINT_SENTINEL == 0) {
+            Serial.print("Turning motor with pin1 = ");
+            Serial.print(pin1, DEC);
+            Serial.print(" , pin2 = ");
+            Serial.print(pin2, DEC);
+            Serial.print(" , and enable pin = ");
+            Serial.print(enable, DEC);
+            //Serial.print(" (" + (enable == enableLeftMotor) ? "left" : (enable == enableRightMotor) ? "right" : "unknown" + ")"); // Concatenation/Type-Strictness issues, my beloved.
+            Serial.print(" with speed = ");
+            Serial.print(speed, DEC);
+            Serial.println();
+        }
+    }
 
-  if (speed < 0)
-  {
-    digitalWrite(pin1,LOW);
-    digitalWrite(pin2,HIGH);    
-  }
-  else if (speed > 0)
-  {
-    digitalWrite(pin1,HIGH);
-    digitalWrite(pin2,LOW);      
-  }
-  else
-  {
-    digitalWrite(pin1,LOW);
-    digitalWrite(pin2,LOW);      
-  }
+    if (speed < 0)
+    {
+        digitalWrite(pin1,LOW);
+        digitalWrite(pin2,HIGH);    
+    }
+    else if (speed > 0)
+    {
+        digitalWrite(pin1,HIGH);
+        digitalWrite(pin2,LOW);      
+    }
+    else
+    {
+        digitalWrite(pin1,LOW);
+        digitalWrite(pin2,LOW);      
+    }
 
-  analogWrite(enable, abs(speed));
+    analogWrite(enable, abs(speed));
 }
 
