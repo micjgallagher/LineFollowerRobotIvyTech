375cc059579b29afd6082b1dce426419f122b6bf
Duncan Brasher <dbrasher5@ivytech.edu>
2025-03-17 10:06:51 -0400

    Make `Serial.begin` call conditional on `DEBUG_PRINT`; Add comment declaring that the *magic line* should stay disabled.

--- LineFollowerRobot/LineFollowerRobot.ino
+++ LineFollowerRobot/LineFollowerRobot.ino
@@ -23,14 +23,16 @@ int leftMotorPin2=10;
 
 void setup()
 {
-    Serial.begin(9600); 
+    if (DEBUG_PRINT) {
+        Serial.begin(9600); 
+    }
     //The problem with TT gear motors is that, at very low pwm value it does not even rotate.
     //If we increase the PWM value then it rotates faster and our robot is not controlled in that speed and goes out of line.
     //For that we need to increase the frequency of analogWrite.
     //Below line is important to change the frequency of PWM signal on pin D5 and D6
     //Because of this, motor runs in controlled manner (lower speed) at high PWM value.
     //This sets frequency as 7812.5 hz.
-    //TCCR0B = TCCR0B & B11111000 | B00000010;
+    //TCCR0B = TCCR0B & B11111000 | B00000010; // Magic line; we had better results with it disabled.
 
     // put your setup code here, to run once:
     pinMode(enableRightMotor, OUTPUT);
